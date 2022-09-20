3748044dd9880851b1dd3f3b06162c5f8d4c9a8f
Denis <denis1993@yandex.ru>
2022-09-20 13:45:17 +0300

    add задачача №4

--- denver02/Program.cs
+++ denver02/Program.cs
@@ -3,4 +3,11 @@
 //вторую цифру этого числа.
 // 456 -> 46     782 -> 72    918 -> 98
 
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int b = int.Parse(a.ToString().Remove(1, 1));
+Console.WriteLine(b);
+
+
+
 
