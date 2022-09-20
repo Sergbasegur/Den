c24b534e6acbaf8265dfd5c27192c60cbe2bb861
Denis <denis1993@yandex.ru>
2022-09-20 16:03:45 +0300

    add задача № 10

---
+++ denver04/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача № 10
+//Напишите программу, которая принимает на вход трехзначное число и на 
+//выходе показывает
+//вторую цифру этого числа.
+//456 -> 5    782 -> 8   918 -> 1
+int a = new Random().Next(100, 1000);
+Console.WriteLine(a);
+int b = int.Parse(a.ToString().Remove(0, 1));
+int c = int.Parse(b.ToString().Remove(1, 1));
+Console.WriteLine(c);
