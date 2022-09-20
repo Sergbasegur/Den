ec4d51c21f6b56bef28cd96162e219a1ad70eb44
Denis <denis1993@yandex.ru>
2022-09-20 14:20:32 +0300

    add задача № 12

--- denver03/Program.cs
+++ denver03/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача № 12
+//Напишите программу, которая будет принимать на вход два числа и
+//вываодить, является ли второе число кратное первому. Если число 2 не кратно 1,
+//то программа выводит остаток деления.
+//34, 5 -> не кратно, остаток 4
+//16, 4 -> кратно
+
+Console.WriteLine("Введите первое число :");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+int b = Convert.ToInt32(Console.ReadLine());
+int number = a / b;
+
+if( number  %  2 == 0) {
+    Console.WriteLine("второе число кратно первому числу");}
+    else
+    {
+    Console.WriteLine("Остаток при делении равняется - " + a % b);
+    }
+
