d178fa328011d336b9fe12d5654526878589d28b
Denis <denis1993@yandex.ru>
2022-09-23 13:08:12 +0300

    добавлена задача № 18

---
+++ denver08/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача № 18
+// Напишите программу, которая по заданному номеру четверти,
+//  показывает диапазон возможных координат точек в этой четверти(X и Y).
+
+Console.WriteLine("Введите номер четверти");
+string hetvert = Console.ReadLine();
+
+String range = FindRange(hetvert);
+Console.WriteLine(range);
+
+String FindRange(string num)
+   {
+    string phrase = "Диапазон координат находится в пределах";
+ if(num == "1")  return $"{phrase} x > 0 и y > 0";  
+ if(num == "2")  return $"{phrase} x < 0 и y > 0";
+ if(num == "3")  return $"{phrase} x < 0 и y < 0";  
+ if(num == "4")  return $"{phrase} x > 0 и y < 0"; 
+  return "0";
+  }
+
+
+
+
+
+
