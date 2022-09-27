20f004dd8a4149cea83ffd846d8e7b1d288ffd9d
Denis <denis1993@yandex.ru>
2022-09-27 16:19:17 +0300

    добавлена задача № 24

---
+++ denver14/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача № 24
+//Напишите программу которая на вход принимает число(А)
+// и выдает сумму чисел от 1 до (А).
+Console.WriteLine("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine());
+int sumNum = SumNumb(number);
+Console.WriteLine(sumNum);
+
+int SumNumb(int num)
+{
+    int sum = default;
+    for(int i = 1; i <= num; i++)
+    {
+            sum = sum + i;
+    }
+    return sum;
+} 
