25a892ab67af1a6b75894503a05b27b5724d63e9
Denis <denis1993@yandex.ru>
2022-09-27 13:42:29 +0300

    Добавлена задача № 23

---
+++ denver12/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача № 23
+//Напишите программу, которая принимает на вход число(N) и выдает
+//таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число - N :");
+int N = Convert.ToInt32(Console.ReadLine());
+for(int i = 0; i <= N; i++)
+{
+    Console.WriteLine($"куб числа {i} = " + i*i*i);
+}
+
