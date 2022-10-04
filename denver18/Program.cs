556ab763552fe5f2570f333d3c0a11022bfec3a2
Denis <denis1993@yandex.ru>
2022-10-04 09:41:03 +0300

    добавлена задача № 34

--- denver18/Program.cs
+++ denver18/Program.cs
@@ -4,23 +4,6 @@
 
 //[345, 897, 568, 234] -> 2
 
-//  int[] array = new int [5];
-// Random ran = new Random();
-
-// int count = 0;
-// for (int i = 0; i < array.Length; i++)
-// {
-//     array[i] = ran.Next(100, 1000);
-
-//     int a = array[i];
-
-//     if(a  % 2 == 0)
-//     {
-//         count++;
-//         Console.WriteLine($"[{a}] --> {count}");
-        
-//     } 
-// }
 
 int M = 5;
 int sum = 0;
@@ -30,9 +13,9 @@ Random rand =new Random();
 for (int i = 0; i < M; i++)
 {
     S[i] = rand.Next(100, 1000);
-    Console.WriteLine(S[i] + " ");
     if(S[i] % 2 == 0) sum++;
     
 }
-Console.WriteLine("количество четных чисел = " + sum);
+string a = String.Join(", ", S);
+Console.WriteLine($"[{a}] --> количество четных чисел = " + sum);
 
