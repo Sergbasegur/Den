f2c6118bedb92bb40543044334a0f3f29fc8a345
Denis <denis1993@yandex.ru>
2022-10-06 12:02:11 +0300

    добавлена задача № 40

---
+++ denver24/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача № 40
+// Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник со сторонами такой длины.
+
+// Теорема о неравенстве треугольника - каждая сторона треугольника
+// меньше сумму двух других сторон
+
+Console.WriteLine("Введите длину отрезка АB");
+int ab = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину BC");
+int bc = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите длину отрезка CA");
+int ca = Convert.ToInt32(Console.ReadLine());
+
+if((ab < bc + ca) && (bc < ca + ab) && (ca < ab + bc) == true)
+{
+   Console.WriteLine($"треугольник со сторонами ab = {ab}, bs = {bc}, ca = {ca} существует");
+}  
+else 
+{
+    Console.WriteLine($"треугольник со сторонами ab = {ab}, bs = {bc}, ca = {ca}  не существует");
+}
+
+
+
+
