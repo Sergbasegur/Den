554a1f2ca0a4d562b886f821a0b2093fb5312448
Denis <denis1993@yandex.ru>
2022-10-06 15:26:56 +0300

    добавлена задача № 42

---
+++ denver25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача № 42
+// Напишите программу, которая будет преобразовывать 
+// десятичное число в двоичное.
+// 45 --> 101101
+// 3 --> 11
+// 2 --> 10
+
+int number = 10;
+ConvertToBin2(number);
+
+void ConvertToBin2(int num)
+{
+    if(num == 0) return;
+    ConvertToBin2(num / 2);
+    Console.Write(num % 2);
+}
+
+
+
