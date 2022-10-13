785002ba8e31b4e9cabbb0f270e54816a3bbc489
Denis <denis1993@yandex.ru>
2022-10-13 10:10:39 +0300

    добавлена задача № 54

--- denver32/Program.cs
+++ denver32/Program.cs
@@ -11,43 +11,43 @@
 // 8 4 4 2
 int[,] array = new int[3,4];
 
-void SortArray(int[,] array)
+void SortArray(int[,] arr)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < arr.GetLength(0); i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      for (int k = 0; k < arr.GetLength(1) - 1; k++)
       {
-        if (array[i, k] < array[i, k + 1])
+        if (arr[i, k] < arr[i, k + 1])
         {
-          int temp = array[i, k + 1];
-          array[i, k + 1] = array[i, k];
-          array[i, k] = temp;
+          int temp = arr[i, k + 1];
+          arr[i, k + 1] = arr[i, k];
+          arr[i, k] = temp;
         }
       }
     }
   }
 }
 
-void CreateArray(int[,] array)
+void CreateArray(int[,] arr)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < arr.GetLength(0); i++)
   {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-      array[i, j] = new Random().Next(0,9);
+      arr[i, j] = new Random().Next(0,9);
     }
   }
 }
 
-void WriteArray(int[,] array)
+void WriteArray(int[,] arr)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
+  for (int i = 0; i < arr.GetLength(0); i++)
     {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int j = 0; j < arr.GetLength(1); j++)
         {
-      Console.Write(array[i, j] + " ");
+      Console.Write(arr[i, j] + " ");
         }
     Console.WriteLine();
     }    
